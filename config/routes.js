b5d5bd130d8dd99eb755b9877b8c429cab0c6883
Marcos Cesar <marcos.cesar@ccc.ufcg.edu.br>
2018-09-11 14:06:49 -0300

    Login e Registro

--- config/routes.js
+++ config/routes.js
@@ -4,6 +4,7 @@ module.exports = function(server) {
 
     const router = express.Router()
     server.use('/api', router)
+    server.use('/login', require('../src/controllers/authController'))
 
     const userService = require('../src/users/userService')
     userService.register(router, '/user')
