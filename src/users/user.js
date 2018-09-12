c34ef62e6128f814a12c9d5f457d6190a788cda5
Marcos Cesar <marcos.cesar@ccc.ufcg.edu.br>
2018-09-12 15:05:07 -0300

    Recuperacao de senha

--- src/users/user.js
+++ src/users/user.js
@@ -6,7 +6,9 @@ const userSchema = new mongoose.Schema({
     name: {type: String, required: true},
     email: {type: String, required: true},
     password: {type: String, required: true},
-    type: {type: String, enum: ['CLIENT', 'DENTIST', 'SECRETARY'], default: 'CLIENT', required: true}
+    type: {type: String, enum: ['CLIENT', 'DENTIST', 'SECRETARY'], default: 'CLIENT', required: true},
+    resetPasswordToken: String,
+    resetPasswordExpires: Date
 })
 
 userSchema.pre('save', async function (next) {
