77e8d7fa3820748749fe408499587961294bbbd0
yago-tavares <yago.tavares@ccc.ufcg.edu.br>
2018-09-17 12:57:32 -0300

    Update port of server

--- config/server.js
+++ config/server.js
@@ -6,7 +6,8 @@ const server = express();
 server.use(bodyParser.urlencoded({extended: true}));
 server.use(bodyParser.json());
 
-server.listen(process.env.port || port, function() {
+
+server.listen(process.env.PORT || 3030, function() {
     console.log('Listening on')
 });
 
