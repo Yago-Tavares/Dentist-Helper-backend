0f91df34c7dbc174414fa2ff39632596985565bc
Marcos Cesar <marcos.cesar@ccc.ufcg.edu.br>
2018-09-18 13:49:55 -0300

    Bugfix: adicionando token na rota de reset de token

--- src/login/login.js
+++ src/login/login.js
@@ -5,7 +5,7 @@ const authController = require('./authController');
 router.post('/authenticate', authController.authenticate);
 router.post('/register', authController.register);
 router.post('/forgot', authController.forgot_password);
-router.post('/reset', authController.reset_password);
+router.post('/reset/:token', authController.reset_password);
 router.put('/password/:id', authController.update_password);
 
 module.exports = router;
