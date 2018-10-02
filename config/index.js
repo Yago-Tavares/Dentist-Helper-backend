918717f886b1ebd7141a5fe5e1e8c502735041dc
Lucas Wilker <lucas.barbosa@ccc.ufcg.edu.br>
2018-10-02 10:57:25 -0300

    Install swagger

--- config/index.js
+++ config/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const login = require('../src/login/routes');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swaggerDoc/swagger.json');
 
 module.exports = function(server) {
-
+    
+    
     server.use('/api', router);
     server.use('/', login);
-
-    const userService = require('../src/users/userService')
-    userService.register(router, '/user')
+    server.use('/api-docs',swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+    // const userService = require('../src/users/userService')
+    // userService.register(router, '/user');
 }
