2272ebf5406b48dd0361ece7ba41caf8d4aa76a2
unknown <marcos.cesar@ccc.ufcg.edu.br>
2018-10-06 17:26:47 -0300

    Schema e CRUD de operations

--- config/index.js
+++ config/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const login = require('../src/login/routes');
+const operation = require('../src/operation/routes');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swaggerDoc/swagger.json');
 
@@ -9,6 +10,7 @@ module.exports = function(server) {
     
     // server.use('/api', router);
     server.use('/', login);
+    server.use('/', operation);
     server.use('/api-docs',swaggerUi.serve, swaggerUi.setup(swaggerDocument));
     // const userService = require('../src/users/userService')
     // userService.register(router, '/user');
