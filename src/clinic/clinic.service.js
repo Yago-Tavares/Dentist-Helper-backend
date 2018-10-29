29ee7cea4523334b6feba15f7231da7ae08ce5b4
Lucas Wilker <lucas.barbosa@ccc.ufcg.edu.br>
2018-10-29 11:58:30 -0300

    /US07: Auto stash before merge of "Hotfix/US07" and "origin/Hotfix/US07"

---
+++ src/clinic/clinic.service.js
@@ -0,0 +1,40 @@
+const Dentist = require('../dentist/dentist');
+const Client = require('../client/client');
+const Secretary = require('../secretary/secretary');
+
+exports.getAllDentists = async (clinicId, callback) =>{
+
+    await Dentist.find({clinic: clinicId}).then((result) => {
+        callback({
+            status: 200,
+            data: result
+        });    
+    }).catch((err) => {
+
+        callback({status:404, data:'Id da clinica não existe'});
+    });;
+}
+
+exports.getAllClients = async (clinicId, callback) =>{
+
+    await Client.find({clinic: clinicId}).then((result) => {
+        callback({
+            status: 200,
+            data: result
+        });    
+    }).catch((err) => {
+        callback({status:404, data:'Id da clinica não existe'});
+    });;
+}
+
+exports.getAllSecretaries = async (clinicId, callback) =>{
+
+    await Secretary.find({clinic: clinicId}).then((result) => {
+        callback({
+            status: 200,
+            data: result
+        });    
+    }).catch((err) => {
+        callback({status:404, data:'Id da clinica não existe'});
+    });;
+}
