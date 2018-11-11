a1b18ff706d939d8b6dfe39e0283cbf0b273a8e4
yago-tavares <yago.tavares@ccc.ufcg.edu.br>
2018-11-11 15:15:17 -0300

    Fix bug of reponse in user service

--- src/users/userService.js
+++ src/users/userService.js
@@ -1,18 +1,18 @@
-const User = require('../users/user.model');
-const response = require('../util/responses');
+// const User = require('../users/user.model');
+// const response = require('../util/responses');
 
-// User.methods(['get', 'post', 'put', 'delete'])
+// // User.methods(['get', 'post', 'put', 'delete'])
 
-// User.updateOptions({new: true, runValidators: true})
+// // User.updateOptions({new: true, runValidators: true})
 
-exports.updateUser = async (userId, update, callback) => {
+// exports.updateUser = async (userId, update, callback) => {
 
-    await User.findByIdAndUpdate({_id: userId}, {$set: update}).then((result) => {
-        if(!result) callback(response.notFound('Usuário não existe'));
-        else callback(response.ok('Atualizado com Succeso!', result));
-    }).catch((err) => {
-        callback(response.badRequest('Não foi possivel atualizar'));
-    });;
-};
+//     await User.findByIdAndUpdate({_id: userId}, {$set: update}).then((result) => {
+//         if(!result) callback(response.notFound('Usuário não existe'));
+//         else callback(response.ok('Atualizado com Succeso!', result));
+//     }).catch((err) => {
+//         callback(response.badRequest('Não foi possivel atualizar'));
+//     });;
+// };
 
-module.exports = user
+// module.exports = user
