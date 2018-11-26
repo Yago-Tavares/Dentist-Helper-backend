5d3040b964ce995b63a597887fde5af26680743a
Nilton Ginani <nilton.ginani@ccc.ufcg.edu.br>
2018-11-26 14:06:12 -0300

    toothSchema, remove type, entrou name com enum

--- src/tooth/tooth.model.js
+++ src/tooth/tooth.model.js
@@ -2,9 +2,12 @@ const restful = require('node-restful');
 const mongoose = restful.mongoose;
 
 const toothSchema =  new mongoose.Schema({
-    type: { 
+    name: { 
         type: String,
-        enum: ['INCISIVO', 'CANINO', 'PRÉ-MOLAR', 'MOLAR'],
+        enum: ['11', '12', '13', '14','15','16','17','18',  // superiores direitos
+               '21', '22', '23', '24','25','26','27','28',  // superiores esquerdos
+               '31', '32', '33', '34','35','36','37','38',  // inferiores direitos
+               '41', '42', '43', '44','45','46','47','48'], // inferiores esquerdos
         required: false
     },
     specification: {
