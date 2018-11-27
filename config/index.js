b53d0bae70c6051c338a4de47e5c8a2c4ef2b247
Yago <yago.tavares@ccc.ufcg.edu.br>
2018-11-27 18:11:44 -0300

    Add router secretary

--- config/index.js
+++ config/index.js
@@ -5,6 +5,7 @@ const procedure = require('../src/procedure/procedure.routes');
 const tooth = require('../src/tooth/tooth.routes');
 const clinic = require('../src/clinic/clinic.routes');
 const dentist = require('../src/dentist/dentist.routes');
+const secretary = require('../src/secretary/secretary.routes');
 const client = require('../src/client/client.routes');
 const user = require('../src/users/user.routes');
 const swaggerUi = require('swagger-ui-express');
@@ -21,6 +22,7 @@ module.exports = function(server) {
     server.use('/', client);
     server.use('/', user);
     server.use('/', tooth);
+    server.use('/', secretary);
 
     server.use('/',swaggerUi.serve, swaggerUi.setup(swaggerDocument));
     // const userService = require('../src/users/userService')
